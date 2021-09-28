6c439d39392e157e34c0d2a2bc83e455b3f7b48a
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-09-28 17:57:56 -0500

    update

--- app/src/App.js
+++ app/src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <div className="container center-h center-v">
-        <div className="child">
-          <code>justify-content: center;<br />align-items: center;</code>
-        </div>
+      <header className="App-header">
+      
+       <div className="child">
+        <code>justify-content: center;<br />align-items: center;</code>
       </div>
+
+      </header>
     </div>
   );
 }
