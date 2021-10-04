205bf33667d6f051885ec718450a91d6e11a1a60
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-04 12:56:41 -0500

    update

--- python/conn.py
+++ python/conn.py
@@ -1,18 +1,23 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt  #import the client1
+import time
 
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
 
-def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
-    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
-    client.subscribe("digitest/test1")  # Subscribe to the topic “digitest/test1”, receive any messages published on it
-
-
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
-    print("Message received-> " + msg.topic + " " + str(msg.payload))  # Print a received msg
-
-
-client = mqtt.Client("mesa1")  # Create instance of client with client ID “digi_mqtt_test”
-client.on_connect = on_connect  # Define callback function for successful connection
-client.on_message = on_message  # Define callback function for receipt of a message
-# client.connect("m2m.eclipse.org", 1883, 60)  # Connect to (broker, port, keepalive-time)
-client.connect('127.0.0.1', 17300)
-client.loop_forever()  # Start networking daemon
+mqtt.Client.connected_flag=False#create flag in class
+broker="192.168.0.13"
+client = mqtt.Client("mesa1")             #create new instance 
+client.on_connect=on_connect  #bind call back function
+client.loop_start()
+print("Connecting to broker ",broker)
+client.connect(broker)      #connect to broker
+while not client.connected_flag: #wait in loop
+    print("In wait loop")
+    time.sleep(1)
+print("in Main Loop")
+client.loop_stop()    #Stop loop 
+client.disconnect() # disconnect
