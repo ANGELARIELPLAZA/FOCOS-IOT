af6f4d8ea7cbc6b90d18a7a83ca62b427ee05759
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-04 13:19:50 -0500

    update

--- python/conn.py
+++ python/conn.py
@@ -1,23 +1,25 @@
-import paho.mqtt.client as mqtt  #import the client1
-import time
+import paho.mqtt.client as mqtt
 
+# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        client.connected_flag=True #set flag
-        print("connected OK")
-    else:
-        print("Bad connection Returned code=",rc)
+    print("Connected with result code "+str(rc))
 
-mqtt.Client.connected_flag=False#create flag in class
-broker="192.168.0.13"
-client = mqtt.Client("mesa1")             #create new instance 
-client.on_connect=on_connect  #bind call back function
-client.loop_start()
-print("Connecting to broker ",broker)
-client.connect(broker)      #connect to broker
-while not client.connected_flag: #wait in loop
-    print("In wait loop")
-    time.sleep(1)
-print("in Main Loop")
-client.loop_stop()    #Stop loop 
-client.disconnect() # disconnect
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt.eclipseprojects.io", 1883, 60)
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
