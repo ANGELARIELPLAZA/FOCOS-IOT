b9f493f12bde69390fb548eca53127d5f10ada2a
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-04 23:09:26 -0500

    Update prueba.py

--- python/prueba.py
+++ python/prueba.py
@@ -1,69 +1,40 @@
-import eventlet
-import json
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, flash, request, url_for
+from datetime import timedelta, datetime
+from time import time
 from flask_mqtt import Mqtt
-from flask_socketio import SocketIO
-from flask_bootstrap import Bootstrap
-
-eventlet.monkey_patch()
+ 
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_URL'] = "localhost"
 app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
-app.config['MQTT_CLEAN_SESSION'] = True
-
-# Parameters for SSL enabled
-# app.config['MQTT_BROKER_PORT'] = 8883
-# app.config['MQTT_TLS_ENABLED'] = True
-# app.config['MQTT_TLS_INSECURE'] = True
-# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
+app.config['MQTT_KEEPALIVE'] =20
 
 mqtt = Mqtt(app)
-socketio = SocketIO(app)
-bootstrap = Bootstrap(app)
 
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print('on_connect client : {} userdata :{} flags :{} rc:{}'.format(client, userdata, flags, rc))
+    mqtt.subscribe("outTopic")
+    mqtt.subscribe("death")
 
-@app.route('/')
-def index():
-    return 'index.html'
-
-
-@socketio.on('publish')
-def handle_publish(json_str):
-    data = json.loads(json_str)
-    mqtt.publish(data['mesa1'], data['0'])
-
-
-@socketio.on('subscribe')
-def handle_subscribe(json_str):
-    data = json.loads(json_str)
-    mqtt.subscribe(data['outTopic'])
-    print(data)
-
-
-@socketio.on('unsubscribe_all')
-def handle_unsubscribe_all():
-    mqtt.unsubscribe_all()
+@mqtt.on_subscribe()
+def handle_subscribe(client, userdata, mid, granted_qos):
+    print('on_subscribe client : {} userdata :{} mid :{} granted_qos:{}'.format(client, userdata, mid, granted_qos))
 
 
 @mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    socketio.emit('mqtt_message', data=data)
+def handle_message(client, userdata, message):
+    print('on_message client : {} userdata :{} message.topic :{} message.payload :{}'.format(
+        client, userdata, message.topic, message.payload.decode()))
 
+@mqtt.on_disconnect()
+def handle_disconnect(client, userdata, rc):
+    print('on_disconnect client : {} userdata :{} rc :{}'.format(client, userdata, rc))
+    
 
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
     print(level, buf)
 
-
-if __name__ == '__main__':
-    # important: Do not use reloader because this will create two Flask instances.
-    # Flask-MQTT only supports running with one instance
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
+app.run(port=5000)
