29b637a62de5ce2c7599a9d7da1442d064e4ca9e
ANGELARIELPLAZA <ielc19.aplazaa@itesco.edu.mx>
2021-10-04 22:46:20 -0500

    Update prueba.py

--- python/prueba.py
+++ python/prueba.py
@@ -1,32 +1,12 @@
 from flask import Flask
-import paho.mqtt.client as mqtt
+from flask_mqtt import Mqtt
 
 app = Flask(__name__)
-
-topic = 'outTopic'
-topic2 = 'mesa1'
-port = 3000
-
-def on_connect(client, userdata, rc):
-    client.subscribe(topic)
-    client.publish(topic2, "STARTING SERVER")
-    client.publish(topic2, "CONNECTED")
-
-
-def on_message(client, userdata, msg):
-    client.publish(topic2, "MESSAGE")
-
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+mqtt = Mqtt(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
-
-if __name__ == '__main__':
-    client = mqtt.Client()
-    #client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect('localhost')
-    client.loop_start()
-
-    app.run(host='0.0.0.0', port=port)
+def index():
+    return 'Hello World!'
